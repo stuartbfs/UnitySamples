46df6bbe82b89eceae08e0b8dcbcba9dd06f3ea0
stuartbfs <stuart@badfeelingstudios.com>
2016-04-10 16:31:26 +0100

    Added a project with horizontal force being applied

---
+++ BouncingBallWall2D/Assets/Scripts/BallBounce.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallBounce : MonoBehaviour {
+
+    private Rigidbody2D rigidBody;
+
+    void Awake ()
+    {
+        if (rigidBody == null)
+        {
+            rigidBody = GetComponent<Rigidbody2D>();
+        }
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigidBody.AddForce(new Vector2(1000, 0), ForceMode2D.Force);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
